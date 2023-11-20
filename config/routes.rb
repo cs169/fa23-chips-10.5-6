c27709b3562eeed80ef919a0a9459ae8b440b4e9
Zeph Thomas <zephthomas@berkeley.edu>
2023-11-20 23:49:46 +0000

    added basic structure for representative show view

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
     resources :representatives, only: [:index]
     resources :representatives do
         resources :news_items, only: %i[index show]
+        get 'show'
         get '/representatives/:representative_id/my_news_item/new' => 'my_news_items#new',
             :as                                                    => :new_my_news_item
         match '/representatives/:representative_id/my_news_item/new', to:  'my_news_items#create',
---
+++ features/step_definitions/representative_profile_steps.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+Given('there is a representative with a profile') do
+  profile = { name: 'Test Representative', ocdid: 'some-ocdid-1', title: 'Governor' }
+  @representative = Representative.find_or_create_by(profile)
+end
+
+When('the user visits the representative profile page') do
+  visit representative_show_path(@representative.id)
+end
+
+Then('they should see the representative\'s profile details') do
+  expect(page).to have_content(@representative.name)
+  expect(page).to have_content(@representative.id)
+  expect(page).to have_content('President')
+end
