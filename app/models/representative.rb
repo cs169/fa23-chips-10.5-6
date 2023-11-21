2f2a4e54075aef02261368331d63b8e000b657c3
Charlotte Le <charlottele@berkeley.edu>
2023-11-21 05:14:55 +0000

    update rep model and start view

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -9,12 +9,25 @@ class Representative < ApplicationRecord
     rep_info.officials.each_with_index do |official, index|
       ocdid_temp = ''
       title_temp = ''
+      street_temp = ''
+      city_temp = ''
+      state_temp = ''
+      zip_temp = ''
+      party_temp = ''
+      photo_url_temp = ''
 
       rep_info.offices.each do |office|
         if office.official_indices.include? index
           title_temp = office.name
           ocdid_temp = office.division_id
+          photo_url_temp = official.photoUrl
+          unless official.address.nil?
+            city_temp = official.address[0].city
+            state_temp = official.address[0].state
+            zip_temp = official.address[0].zip
+          end
         end
+        party_temp = official.party
       end
 
       rep = Representative.find_or_initialize_by(name: official.name, ocdid: ocdid_temp)
