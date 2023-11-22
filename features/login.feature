02fd7bec0bdf0f72cb89a451a6cbe846eab4be86
Zeph Thomas <zephthomas@berkeley.edu>
2023-11-22 04:32:57 +0000

    Added more login tests

--- features/login.feature
+++ features/login.feature
@@ -2,10 +2,18 @@ Feature: Login
 
   Scenario: User can log in with Google
     Given I am on the login page
-    When I press "Sign in with Google"
-    Then I should be redirected
+    When I sign in with Google
+    Then I should be redirected to the home page
 
   Scenario: User can log in with GitHub
     Given I am on the login page
-    When I press "Sign in with GitHub"
-    Then I should be redirected
+    When I sign in with GitHub
+    Then I should be redirected to the home page
+
+  Scenario: User can log out
+    Given I am logged in
+    When I am on the login page
+    Then I should see a login profile
+    Then I can logout
+    Then I should be redirected to the home page
+    And I should no longer be logged in
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,6 +1,50 @@
 # frozen_string_literal: true
 
-Then('I should be redirected') do
-  expected_url = 'http://www.example.com/'
-  expect(current_url).to start_with(expected_url)
+When('I sign in with Google') do
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:google_oauth2] = {
+    'provider' => 'google_oauth2',
+    'uid'      => '12345',
+    'info'     => {
+      'name'  => 'Test User',
+      'email' => 'test@example.com'
+    }
+  }
+  click_button 'Sign in with Google'
+end
+
+When('I sign in with GitHub') do
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:github] = {
+    'provider' => 'github',
+    'uid'      => '12345',
+    'info'     => {
+      'name'  => 'Test User',
+      'email' => 'test@example.com'
+    }
+  }
+  click_button 'Sign in with GitHub'
+end
+
+Then('I should be redirected to the home page') do
+  expect(page).to have_current_path('/')
+end
+
+Given('I am logged in') do
+  step 'I am on the login page'
+  step 'I sign in with Google'
+  step 'I should be redirected to the home page'
+end
+
+Then('I should see a login profile') do
+  expect(page).to have_content('Your Profile')
+end
+
+Then('I can logout') do
+  click_link 'Logout'
+end
+
+And('I should no longer be logged in') do
+  step 'I am on the login page'
+  expect(page).to have_content('Sign In')
 end
