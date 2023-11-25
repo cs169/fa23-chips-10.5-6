b64e4268d0604439a230e6c5b4b1e00d08eede10
Jason Huang <jasonhuang8@berkeley.edu>
2023-11-25 04:19:20 +0000

    removed unecessary previous code

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -4,8 +4,4 @@ class RepresentativesController < ApplicationController
   def index
     @representatives = Representative.all
   end
-
-  def show
-    @representatives = Representative.find(params[:id])
-  end
 end
