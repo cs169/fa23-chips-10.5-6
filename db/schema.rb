201c2d1c56d3d5a2581cb7eaa6be95fbf3511e9b
Charlotte Le <charlottele@berkeley.edu>
2023-12-07 04:33:22 +0000

    re-migrate

--- db/schema.rb
+++ db/schema.rb
@@ -51,8 +51,8 @@ ActiveRecord::Schema.define(version: 2023_11_29_205552) do
 
   create_table "news_items", force: :cascade do |t|
     t.string "title", null: false
-    t.string "issue"
     t.string "link", null: false
+    t.string "issue"
     t.text "description"
     t.integer "representative_id", null: false
     t.datetime "created_at", null: false
